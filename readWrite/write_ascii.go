7f540485cdf8fc297223268d54c10a2a998e9077
doonyango <datieno001@gmail.com>
2024-06-30 01:01:19 +0300

    Feat: Implement create and  write to file

--- readWrite/write_ascii.go
+++ readWrite/write_ascii.go
@@ -1,1 +1,24 @@
 package output
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func WriteAscii(content, flname string) error {
+	if !strings.HasSuffix(flname, ".txt") {
+		return fmt.Errorf("usage: go run . --output=<fileName.txt> something standard")
+	}
+	file, err := os.Create(flname)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.WriteString(file, content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
